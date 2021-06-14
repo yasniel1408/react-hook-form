efb0d7c39cd29461ef7eb0d5d41f8b783219ffa5
Yasniel Fajardo Egues <54322191+yasniel1408@users.noreply.github.com>
2021-06-13 21:02:49 -0400

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+import { DevTool } from "@hookform/devtools";
 
 function App() {
+
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      nombre: "Yasniel",
+      apellidos: "Fajardo Egues",
+      nivel: 5,
+      edad: 26,
+    },
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
+        <Select label="Nivel" {...register("nivel")} />
+
+        <input type="file" {...register("avatar", { required: true })}  accept="image/png, image/jpeg"/>
+        {errors.avatar?.type === "required" && "El avatar es requerido"}
+
+        <input {...register("nombre", { required: true, maxLength: 20 })} />
+        {errors.nombre?.type === "required" && "Nombre es requerido"}
+        {errors.nombre?.type === "maxLength" && "No puede tener mas de 20 caracteres"}
+
+
+        <input {...register("apellidos", { required: true })} />
+        {errors.apellidos?.type === "required" && "Apellidos es requerido"}
+
+        <input type="number" {...register("edad", { min: 18, max: 99 })} />
+        {errors.edad?.type === "min" && "La edad mínima es de 18 años"}
+        {errors.edad?.type === "min" && "La edad máxima es de 99 años"}
+
+        <input type="submit" />
+      </form>
+      <DevTool control={control} />
+    </>
   );
 }
 
 export default App;
+
+const Select = React.forwardRef(({ onChange, onBlur, name, label }, ref) => (
+  <>
+    <label>{label}</label>
+    <select name={name} ref={ref} onChange={onChange} onBlur={onBlur}>
+      <option value="1">Nivel 1</option>
+      <option value="2">Nivel 2</option>
+      <option value="3">Nivel 3</option>
+      <option value="4">Nivel 4</option>
+      <option value="5">Nivel 5</option>
+    </select>
+  </>
+));
